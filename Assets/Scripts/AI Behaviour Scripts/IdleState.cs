6506d191d7890debb956a6977b1f219d026102b6
gabrieleSantona <gabrielesantonauni@gmail.com>
2024-05-11 11:31:30 +0200

    [NEW] Iniziato sistema a stati per l'IA [NEW] Creati IdleState e PatrolState con NavMeshAgent in scena

---
+++ Assets/Scripts/AI Behaviour Scripts/IdleState.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class IdleState : StateMachineBehaviour
+{
+    [Header("Controlli animazione")]
+    [SerializeField] private float stateDuration;  //Indica la durata dello stato
+
+
+    float timer;
+    
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        timer = 0f;
+        
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        timer += Time.deltaTime;
+        if(timer > stateDuration)
+        {
+            animator.SetBool("isPatroling", true);
+        }
+        Debug.Log("idlellando");
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    { 
+    
+    }
+    
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
