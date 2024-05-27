bebb80d022827fde150d1d11f76af49233642866
gabrieleSantona <gabrielesantonauni@gmail.com>
2024-05-27 18:26:37 +0200

    [UPDATE] Modifica Character per stati e effetti

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -43,6 +43,18 @@ public class Character : MonoBehaviour
     public float runSpeed;
     public float atkSpeed;
 
+    //Listone delle cose da controllare
+    private Dictionary<Element, int> statusCharge = new Dictionary<Element, int>();
+    private Dictionary<Element, float> statusTimer = new Dictionary<Element, float>();
+    private Dictionary<Element, int> effectCountdown = new Dictionary<Element, int>();
+    private Dictionary<Element, float> effectTimer = new Dictionary<Element, float>();
+    private Dictionary<Element, bool> effectsApplied = new Dictionary<Element, bool>();
+    private List<Element> elementsOfStatusApplied = new List<Element>();
+    private List<Element> elementsOfEffectsApplied = new List<Element>();
+    private bool isStatusApplied;
+    private bool isEffectApplied;
+
+
     public void Awake()
     {
         stats.atk = atk;
@@ -57,23 +69,59 @@ public class Character : MonoBehaviour
         stats.elemDef.Add(Element.Lightning, lightningDef);
         stats.elemAtk.Add(Element.Earth, earthAtk);
         stats.elemDef.Add(Element.Earth, earthDef);
-        
     }
-    
+
     // Update is called once per frame
     public void Update()
     {
+        //Se il timer di uno qualunque tra effetti o status si azzera, allora fai quello che devi fare
+        foreach (Element element in elementsOfStatusApplied)
+        {
+            if (statusTimer[element] <= 0f)
+            {
+                statusCharge[element] = 0;
+                elementsOfStatusApplied.Remove(element);
+            }
+        }
+        foreach(Element element in elementsOfEffectsApplied)
+        {
+            if (effectTimer[element] <= 0f)
+            {
+                Effect(element);
+            }
+        }
+
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("Damage " + stats.atk);
             if (isPlayer)
             {
-                if(currentHP-(stats.atk-stats.def)>=0)
-                    gameObject.GetComponent<PlayerCharacter>().UpdateHP(currentHP-(stats.atk-stats.def));
+                if (currentHP - (stats.atk - stats.def) >= 0)
+                    gameObject.GetComponent<PlayerCharacter>().UpdateHP(currentHP - (stats.atk - stats.def));
                 else
                     gameObject.GetComponent<PlayerCharacter>().UpdateHP(0);
             }
         }
+
+        //Se la lista degli elementi applicati all'oggetto non è vuota, diminuisci il timer dello status o dell'effetto di quell'elemento
+        if(elementsOfStatusApplied.Count > 0) {
+            foreach (Element element in elementsOfStatusApplied)
+            {
+                if (statusTimer[element] > 0f)
+                    statusTimer[element] -= Time.deltaTime;
+            }
+        }
+        else isStatusApplied = false;
+        if (elementsOfEffectsApplied.Count > 0)
+        {
+            foreach (Element element in elementsOfEffectsApplied)
+            {
+                if (effectTimer[element] > 0f)
+                    effectTimer[element] -= Time.deltaTime;
+            }
+        }
+        else isEffectApplied = false;
         
     }
 
@@ -94,7 +142,7 @@ public class Character : MonoBehaviour
         Debug.Log("Danno inflitto: " + damage);
 
         UpdateHP(currentHP-damage);
-        TakeElementalStatus(element);
+        TakeElementalStatus(damage, element);
         if (currentHP <= 0)
         {
             Die();
@@ -102,12 +150,42 @@ public class Character : MonoBehaviour
     }
     
 
-
-    public void TakeElementalStatus(Element element)
+    //Applica lo status all'oggetto, salvalo nella lista e fai partire il timer
+    public void TakeElementalStatus(int elementalDamage, Element element)
     {
         Debug.Log("Applicazione Stato:" + element);
+
+        if (!effectsApplied[element])
+        {
+            statusCharge[element] += elementalDamage;
+            if(!elementsOfStatusApplied.Contains(element))
+                elementsOfStatusApplied.Add(element);
+            isStatusApplied = true;
+            statusTimer[element] = 5f;
+        }
+        if (statusCharge[element] >= 100 )
+        {
+            ApplyElementEffect(element);
+        }
+        
+    }
+    //Applicato abbastanza status, applica l'effetto e applicalo una volta ogni 5 secondi per 5 volte.
+    public void ApplyElementEffect(Element element)
+    {
+        isEffectApplied = true;
+        effectCountdown[element] = 5;
+        effectTimer[element] = 5f;
+
     }
 
+    //Applica il danno o il malus di quello specifico elemento
+    public void Effect(Element e)
+    {
+
+
+        effectCountdown[e] -= 1;
+        effectTimer[e] = 5f;
+    }
     public virtual void Die()
     {
         // animazione personaggio che muore???
