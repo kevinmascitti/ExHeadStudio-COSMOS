904c0f7f9700a69e0e08a7cd1d6d32e596f91fc0
gabrieleSantona <gabrielesantonauni@gmail.com>
2024-11-12 23:30:46 +0100

    [UPDATE] ultimi fix

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -324,7 +324,7 @@ public class PlayerMovement : MonoBehaviour
        
         if (isGrounded == true && playerVector.y <= 0)
         {
-            player.isInputOn = true;
+            
             isJumpFalling = false;
             //velocity.y = -0.5f; //serve ad essere sicuri che "senta" il terreno, vale come la gravità
             coyoteTimeCounter = coyoteTime;
@@ -391,7 +391,7 @@ public class PlayerMovement : MonoBehaviour
                 /*if(direction.magnitude >= 0.1f)
                {*/
                 isMoving = true;
-                horizontalMovement = new Vector3(moveDir.x, 0f, moveDir.z);
+                horizontalMovement = new Vector3(moveDir.x, 0f, moveDir.z).normalized*movementSpeed;
                 playerController.Move(horizontalMovement * Time.deltaTime);
                 //Debug.Log(moveDir);
                 //}
@@ -434,7 +434,7 @@ public class PlayerMovement : MonoBehaviour
         {
             horizontalMovement.y = playerVector.y;
             playerController.Move(horizontalMovement * Time.fixedDeltaTime);
-            player.isInputOn = false;
+            
         }
         HandleGravity();
         HandleJump();
